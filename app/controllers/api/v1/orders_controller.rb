007647dd71eac9025a610e3eba81e224fe731748
gnardinosaur <github email address>
2019-12-30 18:21:11 -0500

    order create working but will likely need to udpate to real Items since Wegman's API  isn't working out

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,12 +1,19 @@
 class Api::V1::OrdersController < ApplicationController
 
   def index
-      orders = Order.all
-      render json: orders
+    orders = Order.all
+    render json: orders
   end
 
   def show 
-      order = Order.find(params[:id])
-      render json: order
+    order = Order.find(params[:id])
+    render json: order
   end
+
+  def create
+    user = User.find(params[:userId])
+    newOrder = Order.create(items: params[:items], total: params[:total], user: user)
+    render json: newOrder
+  end
+
 end
